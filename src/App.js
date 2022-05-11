e3ba61dc4e966eb5e27a0c34c523b8850d18f9b1
Rmoreno3 <reynaldomoreno173@gmail.com>
2022-05-10 22:14:52 -0400

    Maquetacion del TODO list

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { TodoCounter } from "./components/TodoCounter";
+import { TodoSearch } from "./components/TodoSearch";
+import { TodoList } from "./components/TodoList";
+import {  TodoItem } from "./components/TodoItem";
+import { CreateTodoButton } from "./components/CreateTodoButton";
+// import './App.css';
+
+const todos = [
+  { id: 0, text:'Hacer ejercicio', completed: false },
+  { id: 1, text:'Tomar agua', completed: false },
+  { id: 2, text:'Comer merienda', completed: false },
+]
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <TodoCounter />
+      <TodoSearch />
+      <TodoList>
+        {todos.map(todo => (
+          <TodoItem key={todo.id} text={todo.text} status={todo.completed} /> 
+        ))}
+      </TodoList>
+      <CreateTodoButton />
+    </>
   );
 }
 
